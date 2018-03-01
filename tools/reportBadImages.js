3f6f4ec8591f3d9d0f4ce94f5ead47f2e36dd13c
ZeusTheTrueGod <l-2732@yandex.ru>
2018-03-01 19:46:58 +0300

    Do not allow to keep a low res logo. close #259

--- tools/reportBadImages.js
+++ tools/reportBadImages.js
@@ -2,7 +2,7 @@ const traverse = require('traverse');
 const source = require('js-yaml').safeLoad(require('fs').readFileSync('processed_landscape.yml'));
 const tree = traverse(source);
 tree.map(function(node) {
-  if (node && !node.best_res && node.image_data && node.image_data.low_res) {
+  if (node && node.image_data && node.image_data.low_res) {
     console.info(`Item ${node.name} of ${node.organization} has a low resolution image: ${node.image_data.low_res}`);
     return;
   }

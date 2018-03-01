d04745721a2ac257a3c156b26a9ac034fb20cc00
Dan Kohn <dan@dankohn.com>
2018-03-01 07:15:11 -0500

    Interactive landscape check in
    Signed-off-by: Dan Kohn <dan@dankohn.com>

---
+++ update_server/setup.bash
@@ -0,0 +1,42 @@
+# Just some ideas
+SERVER_ADDRESS=root@147.75.106.211
+nocheck=" -o StrictHostKeyChecking=no "
+ssh $nocheck $SERVER_ADDRESS << 'EOSSH'
+touch /.cloud-warnings.skip
+which git || (
+  apt-get update
+  apt-get -y install git
+)
+git config --global user.email "l-2732@ya.ru"
+git config --global user.name "CNCF-Bot"
+node -v | grep 8.9 || (
+  curl -sL https://deb.nodesource.com/setup_8.x -o nodesource_setup.sh
+  bash nodesource_setup.sh
+  apt-get -y install nodejs
+  apt-get -y install build-essential
+)
+which yarn || (
+  curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+  echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+  apt-get update && apt-get -y install yarn
+)
+echo '
+0 0 * * * root bash -l -c "bash /root/update.sh >> /root/update.log"
+' > /etc/cron.d/updater
+echo '
+  set -e
+  export HOME=/root
+  . /etc/profile
+  rm -rf /repo
+  git clone https://$GITHUB_USER:$GITHUB_TOKEN@github.com/cncf/filterable-landscape /repo
+  cd /repo
+  yarn
+  LEVEL=medium yarn fetch || echo "yarn failed"
+  git add . || echo "nothing to add"
+  git config --global user.email "info@cncf.io"
+  git config --global user.name "CNCF-bot"
+  (git commit -m "Automated update by CNCF-bot" && git push origin HEAD) || echo "can not commit"
+  sleep 3600
+' > /root/update.sh
+EOSSH
+
---
+++ webpack.config.prod.js
@@ -0,0 +1,171 @@
+// For info about this file refer to webpack and webpack-hot-middleware documentation
+// For info on how we're generating bundles with hashed filenames for cache busting: https://medium.com/@okonetchnikov/long-term-caching-of-static-assets-with-webpack-1ecb139adb95#.w99i89nsz
+import branch from 'git-branch';
+import webpack from 'webpack';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+import WebpackMd5Hash from 'webpack-md5-hash';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import path from 'path';
+import FaviconsWebpackPlugin from 'favicons-webpack-plugin';
+import MinifyPlugin from "babel-minify-webpack-plugin";
+
+const currentBranch = require('process').env['BRANCH'] ||  branch.sync();
+console.info('Branch: ', currentBranch);
+const isMainBranch = ['master', 'production', 'staging'].indexOf(currentBranch) !== -1;
+
+const GLOBALS = {
+  'process.env.NODE_ENV': JSON.stringify('production'),
+  'process.env.GA': require('process').env['GA'],
+  __DEV__: false
+};
+
+export default {
+  resolve: {
+    extensions: ['*', '.js', '.jsx', '.json']
+  },
+  devtool: 'source-map', // more info:https://webpack.js.org/guides/production/#source-mapping and https://webpack.js.org/configuration/devtool/
+  entry: path.resolve(__dirname, 'src/index'),
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: '[name].[chunkhash].js'
+  },
+  plugins: [
+    // Hash the files using MD5 so that their names change when the content changes.
+    new WebpackMd5Hash(),
+
+    // Tells React to build in prod mode. https://facebook.github.io/react/downloads.html
+    new webpack.DefinePlugin(GLOBALS),
+
+    // Generate an external css file with a hash in the filename
+    new ExtractTextPlugin('[name].[contenthash].css'),
+    new FaviconsWebpackPlugin('./src/favicon.png'),
+
+    // Generate HTML file that contains references to generated bundles. See here for how this works: https://github.com/ampedandwired/html-webpack-plugin#basic-usage
+    new HtmlWebpackPlugin({
+      template: 'src/index.ejs',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true
+      },
+      inject: true,
+      // Note that you can add custom options here if you need to handle other custom logic in index.html
+      // To track JavaScript errors via TrackJS, sign up for a free trial at TrackJS.com and enter your token below.
+      useRootcause: isMainBranch,
+    }),
+
+    // Minify JS
+    // new webpack.optimize.UglifyJsPlugin({ sourceMap: true }),
+    isMainBranch ? new MinifyPlugin() : null
+  ].filter( x => !!x),
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.ejs$/, loader: 'ejs-loader',
+      },
+      {
+        test: /\.eot(\?v=\d+.\d+.\d+)?$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              name: '[name].[ext]'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000,
+              mimetype: 'application/font-woff',
+              name: '[name].[ext]'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.[ot]tf(\?v=\d+.\d+.\d+)?$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000,
+              mimetype: 'application/octet-stream',
+              name: '[name].[ext]'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000,
+              mimetype: 'image/svg+xml',
+              name: '[name].[ext]'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(jpe?g|png|gif|ico)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]'
+            }
+          }
+        ]
+      },
+      {
+        test: /(\.css|\.scss|\.sass)$/,
+        use: ExtractTextPlugin.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                minimize: true,
+                sourceMap: true
+              }
+            }, {
+              loader: 'postcss-loader',
+              options: {
+                plugins: () => [
+                  require('autoprefixer')
+                ],
+                sourceMap: true
+              }
+            }, {
+              loader: 'sass-loader',
+              options: {
+                includePaths: [path.resolve(__dirname, 'src', 'scss')],
+                sourceMap: true
+              }
+            }
+          ]
+        })
+      }
+    ]
+  }
+};

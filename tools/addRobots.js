0921d1a7ddec665822a67538249ef9398afe04ee
ZeusTheTrueGod <l-2732@yandex.ru>
2018-03-14 22:46:59 +0300

    only for a master branch

--- tools/addRobots.js
+++ tools/addRobots.js
@@ -1,6 +1,6 @@
 import branch from 'git-branch';
 const currentBranch = require('process').env['BRANCH'] ||  branch.sync();
-const isMainBranch = ['master', 'production', 'staging'].indexOf(currentBranch) !== -1;
+const isMainBranch = currentBranch === 'master';
 
 const content = isMainBranch ?
   `

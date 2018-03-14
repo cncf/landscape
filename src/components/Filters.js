a48b93be3a741d34584226f0eb7adb076b2aa28c
Andrey Kozlov <l-2732@yandex.ru>
2018-03-14 22:18:28 +0300

    Remove unused code; closes #377
    * wip
    
    * removing a bit more commented code
    
    * remove the last vcFunder
    
    * remove even more fields
    
    * remove a bit more non required fields

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -2,11 +2,7 @@ import React from 'react';
 import { FormGroup, FormControl, FormLabel } from 'material-ui/Form';
 
 import CncfFilterContainer from './CncfFilterContainer';
-// import StarsFilterContainer from './StarsFilterContainer';
-// import CertifiedKubernetesFilterContainer from './CertifiedKubernetesFilterContainer'
 import LicenseFilterContainer from './LicenseFilterContainer';
-// import MarketCapFilterContainer from './MarketCapFilterContainer';
-// import VCFunderFilterContainer from './VCFunderFilterContainer';
 import OrganizationFilterContainer from './OrganizationFilterContainer';
 import HeadquartersFilterContainer from './HeadquartersFilterContainer';
 import LandscapeFilterContainer from './LandscapeFilterContainer';
@@ -41,17 +37,6 @@ const Filters = () => {
         </FormControl>
       </FormGroup>
 
-      {
-      /*
-      <FormGroup row>
-        <FormControl component="fieldset">
-          <FormLabel component="legend">{fields.marketCap.label}</FormLabel>
-          <MarketCapFilterContainer />
-        </FormControl>
-      </FormGroup>
-      */
-      }
-
       <FormGroup row>
         <FormControl component="fieldset">
           <FormLabel component="legend">{fields.headquarters.label}</FormLabel>
@@ -59,36 +44,6 @@ const Filters = () => {
         </FormControl>
       </FormGroup>
 
-
-      {
-      /*
-      <FormGroup row>
-        <FormControl component="fieldset">
-          <FormLabel component="legend">{fields.stars.label}</FormLabel>
-          <StarsFilterContainer/>
-        </FormControl>
-      </FormGroup>
-      */
-      }
-
-      {
-      // <FormGroup row style={{}}>
-        // <FormControl component="fieldset">
-          // <FormLabel component="legend">{fields.certifiedKubernetes.label}</FormLabel>
-          // <CertifiedKubernetesFilterContainer/>
-        // </FormControl>
-      // </FormGroup>
-      }
-
-
-      {
-      // <FormGroup row>
-        // <FormControl component="fieldset">
-          // <FormLabel component="legend">{fields.vcFunder.label}</FormLabel>
-          // <VCFunderFilterContainer />
-        // </FormControl>
-      // </FormGroup>
-      }
 
 
     </div>;

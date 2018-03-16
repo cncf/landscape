f33a58f92cb64d3b382f8d0b52daeabcf6ec6c60
Andrey Kozlov <l-2732@yandex.ru>
2018-03-16 23:18:04 +0300

    reduce lodash size; closes #380

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -3,6 +3,7 @@
 import branch from 'git-branch';
 import webpack from 'webpack';
 import ExtractTextPlugin from 'extract-text-webpack-plugin';
+var LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
 import WebpackMd5Hash from 'webpack-md5-hash';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import path from 'path';
@@ -39,8 +40,10 @@ export default {
     // Hash the files using MD5 so that their names change when the content changes.
     new WebpackMd5Hash(),
 
+
     // Tells React to build in prod mode. https://facebook.github.io/react/downloads.html
     new webpack.DefinePlugin(GLOBALS),
+     new LodashModuleReplacementPlugin,
 
     // Generate an external css file with a hash in the filename
     new ExtractTextPlugin('[name].[contenthash].css'),

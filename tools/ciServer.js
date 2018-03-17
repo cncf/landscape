8d36150882c915bd920cb38a1427feb8af1f8961
Andrey Kozlov <l-2732@yandex.ru>
2018-03-17 22:34:13 +0300

    Adding basic tests; closes #286
    * Adding a basic test
    
    * keep the headless mode
    
    * testing again

---
+++ tools/ciServer.js
@@ -0,0 +1,32 @@
+// This file configures a web server for testing the production build
+// on your local machine.
+
+import browserSync from 'browser-sync';
+import historyApiFallback from 'connect-history-api-fallback';
+import {chalkProcessing} from './chalkConfig';
+
+/* eslint-disable no-console */
+
+console.log(chalkProcessing('running a dist server on http://localhost:4000 ...'));
+
+// Run Browsersync
+const result = browserSync({
+  port: 4000,
+  ui: {
+    port: 4001
+  },
+  open: false,
+  server: {
+    baseDir: 'dist'
+  },
+
+  files: [
+    'src/*.html'
+  ],
+
+  middleware: [historyApiFallback()]
+});
+setTimeout(function() {
+  process.exit();
+}, 120 * 1000);
+export default result;

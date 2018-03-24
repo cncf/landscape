7cbf88ce71b73c2b49782f1edc74bbbf8fa43692
ZeusTheTrueGod <l-2732@yandex.ru>
2018-03-24 22:15:51 +0300

    support embed.html

--- src/utils/isEmbed.js
+++ src/utils/isEmbed.js
@@ -1,2 +1,2 @@
-const isEmbed = window.location.pathname.indexOf('embed=true') !== -1;
+const isEmbed = window.location.pathname.indexOf('embed=true') !== -1 || window.location.pathname.indexOf('embed=yes') !== -1;
 export default isEmbed;

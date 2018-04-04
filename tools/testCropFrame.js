ca998b70cccc5763687d3a7e4a6e2b7b01482251
ZeusTheTrueGod <l-2732@yandex.ru>
2018-04-04 23:01:38 +0300

    svg2png

--- tools/testCropFrame.js
+++ tools/testCropFrame.js
@@ -1,9 +1,11 @@
 import Jimp from 'jimp';
 import './svgAutocrop';
+const { convert } = require('convert-svg-to-png');
 
 async function main() {
-  const fname = 'hosted_logos/anchore.png';
-  const image = await Jimp.read(fname);
+  const fname = 'hosted_logos/anchore.svg';
+  const png = await convert(require('fs').readFileSync(fname, 'utf-8'), {width: 288, height: 98});
+  const image = await Jimp.read(png);
   console.info(image);
   const viewport = image.autocropSize();
   console.info(viewport);

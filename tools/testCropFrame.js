ac192f059ce61d5017623383eec21384000f4fac
ZeusTheTrueGod <l-2732@yandex.ru>
2018-04-04 23:11:27 +0300

    updating apex so I can see a difference

--- tools/testCropFrame.js
+++ tools/testCropFrame.js
@@ -3,8 +3,8 @@ import './svgAutocrop';
 const { convert } = require('convert-svg-to-png');
 
 async function main() {
-  const fname = 'hosted_logos/anchore.svg';
-  const png = await convert(require('fs').readFileSync(fname, 'utf-8'), {width: 288, height: 98});
+  const fname = 'hosted_logos/apex.svg';
+  const png = await convert(require('fs').readFileSync(fname, 'utf-8'), {width: 360, height: 122});
   const image = await Jimp.read(png);
   console.info(image);
   const viewport = image.autocropSize();

c5c336e3a23483a49c62ad709adb48b6ef68f340
ZeusTheTrueGod <l-2732@yandex.ru>
2018-04-17 21:26:29 +0300

    add a command to check the homepage url

---
+++ tools/checkLinks.js
@@ -0,0 +1,48 @@
+import colors from 'colors';
+import rp from 'request-promise';
+import Promise from 'bluebird';
+const traverse = require('traverse');
+
+const fatal = (x) => colors.red(colors.inverse(x));
+
+async function getLandscapeItems() {
+  const source = require('js-yaml').safeLoad(require('fs').readFileSync('landscape.yml'));
+  const tree = traverse(source);
+  const items = [];
+  tree.map(function(node) {
+    if (!node) {
+      return;
+    }
+    if (node.item !== null) {
+      return;
+    }
+    items.push({homepageUrl: node.homepage_url, name: node.name});
+  });
+  return items;
+}
+
+async function main() {
+  const items = await getLandscapeItems();
+  const errors= [];
+  await Promise.map(items, async function(item) {
+    try {
+      const result = await rp({
+        url: item.homepageUrl,
+        simple: false,
+        resolveWithFullResponse: true
+      });
+      if (result.statusCode !== 200) {
+        errors.push(`${item.name} has an url ${item.homepageUrl} and the response code is ${result.statusCode}`);
+        require('process').stdout.write(fatal("F"));
+      } else {
+        require('process').stdout.write(".");
+      }
+    } catch (ex) {
+        errors.push(`${item.name} has an url ${item.homepageUrl} and the reason is ${ex.message.substring(0, 200)}`);
+        require('process').stdout.write(fatal("F"));
+    }
+  }, {concurrency: 20});
+  require('process').stdout.write("\n");
+  errors.forEach(console.info);
+}
+main();

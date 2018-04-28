2d93a754573c6811a79d44a078df1f6eaf63d4fd
Dan Kohn <dan@dankohn.com>
2018-04-28 18:46:06 -0400

    Change ad to Seattle
    Signed-off-by: Dan Kohn <dan@dankohn.com>

--- src/components/Ad.js
+++ src/components/Ad.js
@@ -3,7 +3,7 @@ import { OutboundLink } from 'react-ga';
 
 const Ad = () => {
 
-  const url = "https://events.linuxfoundation.org/events/kubecon-cloudnativecon-europe-2018/?utm_source=interactive_landscape&utm_medium=web&utm_campaign=kccnceu18";
+  const url = "https://events.linuxfoundation.org/events/kubecon-cloudnativecon-north-america-2018/?utm_source=interactive_landscape&utm_medium=web&utm_campaign=kccncna18";
   const imgsrc = "/images/kubecon.jpg";
 
   return <OutboundLink className="sidebar-event"

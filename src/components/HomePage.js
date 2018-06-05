892a7bbe6dcb778d559767d6459a5432c619222a
Dan Kohn <dan@dankohn.com>
2018-06-05 14:55:58 -0400

    Extra space
    Signed-off-by: Dan Kohn <dan@dankohn.com>

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -154,7 +154,7 @@ const HomePage = ({isEmbed, ready, hasSelectedItem, filtersVisible, hideFilters,
           { !isEmbed && <div className="disclaimer">
             <h1>CNCF Cloud Native Interactive Landscape</h1>
             CNCF&apos;s <a target="_blank" href="https://github.com/cncf/landscape/blob/master/README.md#trail-map">Cloud Native Trail Map</a>
-            provides a good introduction. You can also view the static <a
+             provides a good introduction. You can also view the static <a
             target="_blank" href="https://github.com/cncf/landscape/blob/master/README.md#current-version">landscape</a> and <a
             target="_blank" href="https://github.com/cncf/landscape/blob/master/README.md#serverless">serverless</a> landscapes.
             Please <a target="_blank" href="https://github.com/cncf/landscape">open</a> a pull request to

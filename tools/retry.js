a4d500fb2c3b0b8f38997e95284291f6e4951e04
ZeusTheTrueGod <l-2732@yandex.ru>
2018-07-06 18:59:17 +0300

    adding a retry attempt when something goes wrong

--- tools/retry.js
+++ tools/retry.js
@@ -1,11 +1,11 @@
 import Promise from 'bluebird';
 const maxAttempts = 5;
-const delay = 50000;
-const retry  = async function (fn, attempts = maxAttempts) {
+const retry  = async function (fn, attempts = maxAttempts, delay = 50000) {
   try {
     const result = await fn();
     return result;
   } catch (ex) {
+    console.info('Attempt #', attempts);
     if (attempts <= 0 ) {
       throw ex;
     }

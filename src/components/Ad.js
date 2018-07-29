4336f8742471bbce9c4a5bf7b99807c226daeaa1
Dan Kohn <dan@dankohn.com>
2018-07-29 15:01:59 -0400

    Use Chinese KubeCon site
    Signed-off-by: Dan Kohn <dan@dankohn.com>

--- src/components/Ad.js
+++ src/components/Ad.js
@@ -3,7 +3,7 @@ import { OutboundLink } from 'react-ga';
 
 const Ad = () => {
 
-  const url = "https://www.lfasiallc.com/events/kubecon-cloudnativecon-china-2018/?utm_source=interactive_landscape&utm_medium=web&utm_campaign=kccncch18";
+  const url = "https://events.linuxfoundation.cn/events/kubecon-cloudnativecon-china-2018/?utm_source=interactive_landscape&utm_medium=web&utm_campaign=kccncch18";
   const imgsrc = "/images/kubecon.jpg";
   const url2 = "https://events.linuxfoundation.org/events/kubecon-cloudnativecon-north-america-2018/?utm_source=interactive_landscape&utm_medium=web&utm_campaign=kccncna18";
   const imgsrc2 = "/images/kubecon2.jpg";

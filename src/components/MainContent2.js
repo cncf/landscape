5c42d72776fef73554646a6b877aa2f22bcc51a3
ZeusTheTrueGod <l-2732@yandex.ru>
2018-08-12 14:27:55 +0300

    support large images

--- src/components/MainContent2.js
+++ src/components/MainContent2.js
@@ -5,29 +5,60 @@ const categoryHeight = 5;
 const itemWidth = 30;
 const itemHeight = 30;
 
-const drawItem = function({item, x, y}) {
-  return <div style={{ position: 'absolute', left: itemWidth * x, top: itemHeight * y, border: '1px solid green', width: itemWidth, height: itemHeight }}>
-    <img src={item.href} style={{ maxHeight:'25px', maxWidth: '25px'}} />
+const drawItem = function({item, x, y, isLarge}) {
+  const k = isLarge ? 2 : 1;
+  return <div style={{ position: 'absolute', left: itemWidth * x, top: itemHeight * y, border: '1px solid green', width: itemWidth  * k, height: itemHeight * k }}>
+    <img src={item.href} style={{ maxHeight: itemWidth * k - 5, maxWidth:itemHeight * k - 5}} />
   </div>;
 }
 
 const drawSubcategory = function(subcategory) {
-  const cols = Math.ceil(subcategory.items.length / categoryHeight );
+  const total = _.sumBy(subcategory.items, function(item) {
+    return item.cncfProject ? 4 : 1;
+  });
+  const items = _.orderBy(subcategory.items, (x) => !x.cncfProject);
+  const cols = Math.ceil(total / categoryHeight );
   const width = itemWidth * cols;
   const height = itemHeight * categoryHeight;
-  return <div style={{ border: '1px solid black', width: width, height: height, position: 'relative' }}>
-    { subcategory.items.map(function(item, index) {
-      return drawItem({item, y: Math.trunc(index / cols), x: index % cols })
+  let x = 0;
+  let y = 0;
+  let busy = {};
+  return <div style={{ width: width + 20, height: height, position: 'relative' }}>
+    { items.map(function(item) {
+      const isLarge = !!item.cncfProject;
+      const result = {item, y: y, x: x, isLarge: isLarge};
+      busy[`${x}:${y}`] = true;
+      if (isLarge) {
+        busy[`${x + 1}:${y}`] = true;
+        busy[`${x}:${y+1}`] = true;
+        busy[`${x + 1}:${y+1}`] = true;
+      }
+      while(busy[`${x}:${y}`]) {
+        x += 1;
+        if (x >= cols) {
+          x = 0;
+          y += 1;
+        }
+      }
+
+      return drawItem(result);
     }) }
   </div>
 };
 
+const drawSeparator = function() {
+  return <div style={{ right: 10, top: 15, bottom: 5, background: 'black', width: 1, position: 'absolute' }}></div>
+
+}
+
 const drawCategory = function({header, subcategories}) {
   return (<div style={{}}>
     <div style={{position: 'relative', height: '200px', margin: '5px', width: '1200px', background: 'lightblue'}} ><div style={{transform: 'rotate(-90deg)', width: '200px', height: '30px', top: '85px', left: '-85px', textAlign: 'center', position: 'absolute', background: 'red'}}>{header}</div>
-      {subcategories.map(function(subcategory) {
-        return <div style={{position: 'relative', left: '40px', display: 'inline-block', fontSize: '8px'}}><span>{subcategory.name}</span>
+      <div style={{width: 40, display: 'inline-block'}} />
+      {subcategories.map(function(subcategory, index, all) {
+        return <div style={{position: 'relative', display: 'inline-block', fontSize: '8px'}}><span>{subcategory.name}</span>
           { drawSubcategory(subcategory) }
+          { index !== all.length - 1 && drawSeparator() }
         </div>
       })}
 

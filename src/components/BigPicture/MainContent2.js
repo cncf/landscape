e99f4935d70c1e6e64a0797bbbab0944864fda67
ZeusTheTrueGod <l-2732@yandex.ru>
2018-08-25 20:58:52 +0300

    adjusting the layout

--- src/components/BigPicture/MainContent2.js
+++ src/components/BigPicture/MainContent2.js
@@ -16,16 +16,16 @@ const MainContent2 = ({groupedItems, onSelectItem }) => {
   const cat6 = _.find(groupedItems, {key: 'Platform'});
   const cat7 = _.find(groupedItems, {key: 'Observability and Analysis'});
   const cat8 = _.find(groupedItems, {key: 'Special'});
-  return <div style={{position: 'relative', width: 1470}}>
-    <HorizontalCategory {...cat1} rows={6} width={980} height={230} top={0} left={0} color="lightgreen" onSelectItem={onSelectItem} />
-    <HorizontalCategory {...cat2} rows={4} width={980} height={160} top={240} left={0} color="lightblue" onSelectItem={onSelectItem} />
-    <HorizontalCategory {...cat3} rows={4} width={980} height={160} top={410} left={0} color="violet" onSelectItem={onSelectItem}/>
-    <HorizontalCategory {...cat4} rows={4} width={980} height={160} top={580} left={0} color="green" onSelectItem={onSelectItem}/>
-    <HorizontalCategory {...cat5} rows={4} width={300} height={160} top={750} left={0} color="darkblue" onSelectItem={onSelectItem} />
-    <VerticalCategory {...cat6} cols={6} width={240} height={700} top={0} left={1000} color="blue" onSelectItem={onSelectItem} />
-    <VerticalCategory {...cat7} cols={5} width={200} height={700} top={0} left={1250} color="lightblue" onSelectItem={onSelectItem} />
-    <HorizontalCategory {...cat8} rows={4} width={740} height={160} top={750} left={710} color="darkblue" onSelectItem={onSelectItem} />
-    <LandscapeInfo width={390} height={160} top={750} left={310} />
+  return <div style={{position: 'relative', width: 1500}}>
+    <HorizontalCategory {...cat1} rows={6} width={1010} height={230} top={0} left={0} color="lightgreen" onSelectItem={onSelectItem} />
+    <HorizontalCategory {...cat2} rows={3} width={1010} height={140} top={240} left={0} color="lightblue" onSelectItem={onSelectItem} />
+    <HorizontalCategory {...cat3} rows={3} width={1010} height={140} top={390} left={0} color="violet" onSelectItem={onSelectItem}/>
+    <HorizontalCategory {...cat4} rows={3} width={1010} height={160} top={550} left={0} color="green" onSelectItem={onSelectItem}/>
+    <HorizontalCategory {...cat5} rows={4} width={300} height={160} top={720} left={0} color="darkblue" onSelectItem={onSelectItem} />
+    <VerticalCategory {...cat6} cols={6} width={240} height={705} top={0} left={1030} color="blue" onSelectItem={onSelectItem} />
+    <VerticalCategory {...cat7} cols={5} width={200} height={705} top={0} left={1280} color="lightblue" onSelectItem={onSelectItem} />
+    <HorizontalCategory {...cat8} rows={4} width={740} height={160} top={720} left={730} color="darkblue" onSelectItem={onSelectItem} />
+    <LandscapeInfo width={390} height={160} top={720} left={320} />
   </div>
 };
 

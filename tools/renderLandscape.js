f2dea348012bb22d1239e37c6bcf71d02ecb2745
ZeusTheTrueGod <l-2732@yandex.ru>
2018-08-28 19:07:20 +0300

    typo

--- tools/renderLandscape.js
+++ tools/renderLandscape.js
@@ -3,7 +3,7 @@ async function main() {
   const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
   const page = await browser.newPage();
   page.setViewport({ width: 6120, height: 3960 })
-  await page.goto('http://localhost:3000/landscape');
+  await page.goto('http://localhost:4000/landscape');
   await page.screenshot({ path: 'src/images/landscape.png', fullPage: false });
   await browser.close();
 }

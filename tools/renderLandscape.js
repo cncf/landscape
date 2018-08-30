1d877be67c16de4128343d4b77b7f205019961be
ZeusTheTrueGod <l-2732@yandex.ru>
2018-08-30 13:10:22 +0300

    back the 4000 port

--- tools/renderLandscape.js
+++ tools/renderLandscape.js
@@ -4,7 +4,7 @@ async function main() {
   const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
   const page = await browser.newPage();
   page.setViewport({ width: 6400, height: 3960 })
-  await page.goto('http://localhost:3000/landscape');
+  await page.goto('http://localhost:4000/landscape');
   await Promise.delay(10000);
   await page.screenshot({ path: 'src/images/landscape.png', fullPage: false });
   await browser.close();

8bc452db419e19c06496c4095536a076aaf301d9
ZeusTheTrueGod <l-2732@yandex.ru>
2018-08-31 23:28:58 +0300

    make switch buttons as links

--- src/components/BigPicture/SwitchButton.js
+++ src/components/BigPicture/SwitchButton.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Tabs from 'material-ui/Tabs/Tabs';
 import Tab from 'material-ui/Tabs/Tab';
+import InternalLink from '../InternalLink';
 import isMobile from '../../utils/isMobile';
 
 
-const SwitchButton = function({mainContentMode, changeMainContentMode}) {
+const SwitchButton = function({mainContentMode, changeMainContentMode, cardUrl, landscapeUrl, serverlessUrl}) {
   if (isMobile) {
     return null;
   }
@@ -15,9 +16,9 @@ const SwitchButton = function({mainContentMode, changeMainContentMode}) {
           style={{marginLeft: 5}}
           onChange={(_event, value) => changeMainContentMode(value)}
         >
-          <Tab label="Card Mode" value="card" />
-          <Tab label="Landscape" value="landscape" />
-          <Tab label="Serverless" value="serverless" />
+          <Tab label="Card Mode" component={(props) => <InternalLink to={cardUrl} {...props}></InternalLink>} value="card"/>
+          <Tab label="Landscape" component={(props) => <InternalLink to={landscapeUrl} {...props}></InternalLink>} value="landscape"/>
+          <Tab label="Serverless" component={(props) => <InternalLink to={serverlessUrl} {...props}></InternalLink>} value="serverless"/>
         </Tabs>
 }
 export default SwitchButton;
--- src/components/BigPicture/SwitchButtonContainer.js
+++ src/components/BigPicture/SwitchButtonContainer.js
@@ -1,10 +1,14 @@
 import { connect } from 'react-redux';
 import SwitchButton from './SwitchButton';
 import { changeMainContentMode } from '../../reducers/mainReducer.js';
+import { filtersToUrl } from '../../utils/syncToUrl';
 
 
 const mapStateToProps = (state) => ({
-  mainContentMode: state.main.mainContentMode
+  mainContentMode: state.main.mainContentMode,
+  cardUrl: filtersToUrl({filters: state.main.filters, grouping: state.main.grouping, sortField: state.main.sortField, mainContentMode: 'card'}),
+  landscapeUrl: filtersToUrl({filters: state.main.filters, grouping: state.main.grouping, sortField: state.main.sortField, mainContentMode: 'landscape'}),
+  serverlessUrl: filtersToUrl({filters: state.main.filters, grouping: state.main.grouping, sortField: state.main.sortField, mainContentMode: 'serverless'}),
 });
 const mapDispatchToProps = {
   changeMainContentMode: changeMainContentMode

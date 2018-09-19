ea756e31546910496f4ef182854f01999e1661d5
ZeusTheTrueGod <l-2732@yandex.ru>
2018-09-19 16:33:50 +0300

    cache github api requests

--- tools/getRepositoryInfo.js
+++ tools/getRepositoryInfo.js
@@ -1,6 +1,10 @@
 import shortRepoName from '../src/utils/shortRepoName';
 import rp from './rpRetry';
+const cache = {};
 export default async function getRepositoryInfo(url) {
+  if (cache[url]) {
+    return cache[url];
+  }
   const repoName = shortRepoName(url);
   const apiUrl = `https://api.github.com/repos/${repoName}?access_token=${process.env.GITHUB_KEY}`;
   const apiInfo = await rp({
@@ -10,6 +14,7 @@ export default async function getRepositoryInfo(url) {
       'User-Agent': 'cncf updater'
     }
   });
+  cache[url] = apiInfo;
   return apiInfo;
 
 }

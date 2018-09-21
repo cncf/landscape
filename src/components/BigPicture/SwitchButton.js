6bb30730d7cf105c85bd39b29955b819955a1349
ZeusTheTrueGod <l-2732@yandex.ru>
2018-09-21 17:20:42 +0300

    more progress

--- src/components/BigPicture/SwitchButton.js
+++ src/components/BigPicture/SwitchButton.js
@@ -6,13 +6,14 @@ import InternalLink from '../InternalLink';
 
 const SwitchButton = function({mainContentMode, changeMainContentMode, cardUrl, landscapeUrl, serverlessUrl}) {
   return <Tabs
+          className="big-picture-switch"
           value={mainContentMode}
           indicatorColor="primary"
           textColor="primary"
           style={{marginLeft: 5, fontSize: '9px', textTransform: ''}}
           onChange={(_event, value) => changeMainContentMode(value)}
         >
-          <Tab label="Card Mode" style={{textTransform: '', fontSize: '9px'}} component={(props) => <InternalLink to={cardUrl} {...props}></InternalLink>} value="card"/>
+          <Tab label="Card Mode" style={{textTransform: '', fontSize: '9px'}} component={(props) => <InternalLink to={cardUrl} style= {{...props.style, fontSize: '8px'}} {...props}></InternalLink>} value="card"/>
           <Tab label="Landscape" style={{textTransform: '', fontSize: '9px'}} component={(props) => <InternalLink to={landscapeUrl} {...props}></InternalLink>} value="landscape"/>
           <Tab label="Serverless" style={{textTransform: '', fontSize: 9}} component={(props) => <InternalLink to={serverlessUrl} {...props}></InternalLink>} value="serverless"/>
         </Tabs>

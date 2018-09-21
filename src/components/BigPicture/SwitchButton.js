1f188c4e09988d48f5079bb4ecc384ba0f7149fc
ZeusTheTrueGod <l-2732@yandex.ru>
2018-09-21 15:20:20 +0300

    first changes to better support big picture on a mobile phone

--- src/components/BigPicture/SwitchButton.js
+++ src/components/BigPicture/SwitchButton.js
@@ -2,23 +2,19 @@ import React from 'react';
 import Tabs from 'material-ui/Tabs/Tabs';
 import Tab from 'material-ui/Tabs/Tab';
 import InternalLink from '../InternalLink';
-import isMobile from '../../utils/isMobile';
 
 
 const SwitchButton = function({mainContentMode, changeMainContentMode, cardUrl, landscapeUrl, serverlessUrl}) {
-  if (isMobile) {
-    return null;
-  }
   return <Tabs
           value={mainContentMode}
           indicatorColor="primary"
           textColor="primary"
-          style={{marginLeft: 5}}
+          style={{marginLeft: 5, fontSize: '9px', textTransform: ''}}
           onChange={(_event, value) => changeMainContentMode(value)}
         >
-          <Tab label="Card Mode" style={{textTransform: ''}} component={(props) => <InternalLink to={cardUrl} {...props}></InternalLink>} value="card"/>
-          <Tab label="Landscape" style={{textTransform: ''}} component={(props) => <InternalLink to={landscapeUrl} {...props}></InternalLink>} value="landscape"/>
-          <Tab label="Serverless" style={{textTransform: ''}} component={(props) => <InternalLink to={serverlessUrl} {...props}></InternalLink>} value="serverless"/>
+          <Tab label="Card Mode" style={{textTransform: '', fontSize: '9px'}} component={(props) => <InternalLink to={cardUrl} {...props}></InternalLink>} value="card"/>
+          <Tab label="Landscape" style={{textTransform: '', fontSize: '9px'}} component={(props) => <InternalLink to={landscapeUrl} {...props}></InternalLink>} value="landscape"/>
+          <Tab label="Serverless" style={{textTransform: '', fontSize: 9}} component={(props) => <InternalLink to={serverlessUrl} {...props}></InternalLink>} value="serverless"/>
         </Tabs>
 }
 export default SwitchButton;

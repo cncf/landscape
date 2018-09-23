ad23489e27c6f4539754615074632852523ec229
ZeusTheTrueGod <l-2732@yandex.ru>
2018-09-23 15:07:40 +0300

    increase a z-index of zoom buttons

--- src/components/BigPicture/ZoomButtons.js
+++ src/components/BigPicture/ZoomButtons.js
@@ -6,7 +6,7 @@ import Button from 'material-ui/Button';
 import React from 'react';
 
 const ZoomButtons = function({canZoomIn, canZoomOut, zoomText, onZoomIn, onZoomOut, onZoomReset}) {
-  return <div className="zoom-buttons" style={{position:'absolute', top: -40, right: 0, zIndex: 1}}>
+  return <div className="zoom-buttons" style={{position:'absolute', top: -40, right: 0, zIndex: 2}}>
         <IconButton disabled={!canZoomOut} onClick={onZoomOut}>
           <Icon>remove_circle</Icon>
         </IconButton>

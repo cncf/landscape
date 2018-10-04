c1a4c2e2d350e7d912f0380baafd960a4e28770b
ZeusTheTrueGod <l-2732@yandex.ru>
2018-10-04 23:00:32 +0300

    removing changes required for debugging

--- tools/addRobots.js
+++ tools/addRobots.js
@@ -1,6 +1,6 @@
 import branch from 'git-branch';
 const currentBranch = require('process').env['BRANCH'] ||  branch.sync();
-const isMainBranch = true;
+const isMainBranch = currentBranch === 'master';
 
 const content = isMainBranch ?
   `

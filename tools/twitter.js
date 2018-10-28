1932b8fe30d6008ebfad78f7ab709e3b6a0ff174
ZeusTheTrueGod <l-2732@yandex.ru>
2018-10-28 23:14:09 +0300

    do not allow a twitter ending with /

--- tools/twitter.js
+++ tools/twitter.js
@@ -61,6 +61,9 @@ export async function extractSavedTwitterEntries() {
 
 async function readDateOriginal(url) {
   await Promise.delay(100); // rate limit
+  if (url.split('/').slice(-1)[0] === '') {
+    throw new Error(`wrong url: ${url} ends with /`);
+  }
   const lastPart = url.split('/').slice(-1)[0];
   const [screenName, extraPart] = lastPart.split('?');
   if (extraPart) {

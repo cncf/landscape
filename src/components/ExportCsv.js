9a71eafe165f60025cf9f7f4c7598d749f3d1b2e
ZeusTheTrueGod <l-2732@yandex.ru>
2018-11-11 21:07:07 +0300

    fix css in the left panel

--- src/components/ExportCsv.js
+++ src/components/ExportCsv.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import SystemUpdateIcon from '@material-ui/icons/SystemUpdate';
-import IconButton from '@material-ui/core/IconButton';
 
 const ExportCsv = ({onExport}) => {
   return (
-    <IconButton disableRipple style={{ color:'#366fa8', fontSize:'0.8em', width:'100%', justifyContent:'flex-start' }} onClick={()=>onExport()} aria-label="Download as CSV">
-      <SystemUpdateIcon style={{ fontSize:'1.2em'}} />&nbsp; Download as CSV
-    </IconButton>
+    <div className="filters-action" onClick={()=>onExport()} aria-label="Download as CSV">
+      <SystemUpdateIcon /><span>Download as CSV</span>
+    </div>
   );
 };
 export default ExportCsv;
--- src/components/ResetFilters.js
+++ src/components/ResetFilters.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import IconButton from '@material-ui/core/IconButton';
 import ResetIcon from '@material-ui/icons/SettingsBackupRestore';
 
 const Filters = ({reset}) => {
   return (
-    <IconButton className="resetfilters" disableRipple style={{ color:'#366fa8', fontSize:'0.8em', width:'100%', justifyContent:'flex-start' }} onClick={()=>reset()} aria-label="Reset Filters">
-      <ResetIcon style={{ fontSize:'1.2em'}}/> Reset Filters
-    </IconButton>
+    <div className="filters-action" onClick={()=>reset()} aria-label="Reset Filters">
+      <ResetIcon /><span>Reset Filters</span>
+    </div>
   );
 };
 export default Filters;

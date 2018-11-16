d014bc8b707cf357a84cd98c724df17650106d37
Dan Kohn <dan@linuxfoundation.org>
2018-11-17 05:25:34 +0800

    Taylorwaggoner update kccnc links (#973)
    * Add KCCNC Barcelona image
    
    * Update KCCNC ads
    
    Signed-off-by: Dan Kohn <dan@dankohn.com>

--- src/components/Ad.js
+++ src/components/Ad.js
@@ -3,10 +3,10 @@ import { OutboundLink } from 'react-ga';
 
 const Ad = () => {
 
-  const url = "https://events.linuxfoundation.cn/events/kubecon-cloudnativecon-china-2018/?utm_source=interactive_landscape&utm_medium=web&utm_campaign=kccncch18";
-  const imgsrc = "/images/kubecon.jpg";
-  const url2 = "https://events.linuxfoundation.org/events/kubecon-cloudnativecon-north-america-2018/?utm_source=interactive_landscape&utm_medium=web&utm_campaign=kccncna18";
-  const imgsrc2 = "/images/kubecon2.jpg";
+  const url = "https://events.linuxfoundation.org/events/kubecon-cloudnativecon-north-america-2018/?utm_source=interactive_landscape&utm_medium=web&utm_campaign=kccncna18";
+  const imgsrc = "/images/kccncna18.jpg";
+  const url2 = "https://events.linuxfoundation.org/events/kubecon-cloudnativecon-europe-2019//?utm_source=interactive_landscape&utm_medium=web&utm_campaign=kccnceu19";
+  const imgsrc2 = "/images/kccnceu19.jpg";
 
   return <div id="kubecon">
   <OutboundLink className="sidebar-event"

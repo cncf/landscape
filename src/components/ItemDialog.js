49231d220430e44ca7bc03ab170f9d279983d2c6
Andrey Kozlov <l-2732@yandex.ru>
2018-11-16 11:35:34 +0300

    dialog opening animation via material ui, close #282 (#968)
    * Starting a dialog with a simple fade animation
    
    * just a 500 delay to make it looking more natural
    
    * horizontal flip
    
    * a bit better transition
    
    * final changes to make it looking awesome
    
    * fixes for an iphone; keeping content visible on closing a dialog
    
    * one more fix for iPhones
    
    * ensure that we get a failure when it does not work on iPhone X
    
    * A proper version where everything works
    
    * do not change data in this PR
    
    * prepare to merge to the master
    
    * just keep a simple fade in / fade out animation
    
    * fix iPhone issues with extra next/prev buttons
    
    * get it working for a landscape layout too

--- src/components/ItemDialog.js
+++ src/components/ItemDialog.js
@@ -7,33 +7,38 @@ import ItemDialogButtonsContainer from './ItemDialogButtonsContainer';
 import '../styles/itemModal.scss';
 import isIphone from '../utils/isIphone';
 
+let lastItemInfo;
 const ItemDialog = ({onClose, itemInfo}) => {
-  if (!itemInfo) {
-    return null;
+  const recentItemInfo = itemInfo || lastItemInfo || {};
+  if (itemInfo) {
+    lastItemInfo = itemInfo;
   }
   if (isIphone) {
+    if (!itemInfo) {
+      return null;
+    }
     return (
-      <div className={classNames('modal', 'product', {sandbox : itemInfo.cncfRelation ==='sandbox'},
-          {incubating : itemInfo.cncfRelation ==='incubating'},
-          {graduated : itemInfo.cncfRelation ==='graduated'},
-          {nonoss : itemInfo.oss === false})}
+      <div className={classNames('modal', 'product', {sandbox : recentItemInfo.cncfRelation ==='sandbox'},
+          {incubating : recentItemInfo.cncfRelation ==='incubating'},
+          {graduated : recentItemInfo.cncfRelation ==='graduated'},
+          {nonoss : recentItemInfo.oss === false})}
         >
           { /* Note - we move buttons away from here to the HomePage because of Safari Issues */ }
-          <ItemDialogContent itemInfo={itemInfo}/>
+          { <ItemDialogContent itemInfo={itemInfo || lastItemInfo}/> }
         </div>
     )
   }
   return (
-    <Dialog open={true} onClose={() => onClose()}
-      classes={{paper:'modal-body'}}
-      className={classNames('modal', 'product', {sandbox : itemInfo.cncfRelation ==='sandbox'},
-                                                 {incubating : itemInfo.cncfRelation ==='incubating'},
-                                                 {graduated : itemInfo.cncfRelation ==='graduated'},
-                                                 {nonoss : itemInfo.oss === false})}
-      >
-        <ItemDialogButtonsContainer/>
-        <ItemDialogContent itemInfo={itemInfo}/>
-    </Dialog>
+      <Dialog open={!!itemInfo} onClose={() => onClose() } transitionDuration={400}
+        classes={{paper:'modal-body'}}
+        className={classNames('modal', 'product', {sandbox : recentItemInfo.cncfRelation ==='sandbox'},
+                                                  {incubating : recentItemInfo.cncfRelation ==='incubating'},
+                                                  {graduated : recentItemInfo.cncfRelation ==='graduated'},
+          {nonoss : recentItemInfo.oss === false})}
+        >
+          { itemInfo && <ItemDialogButtonsContainer/> }
+          { (itemInfo || lastItemInfo) && <ItemDialogContent itemInfo={itemInfo || lastItemInfo}/> }
+      </Dialog>
   );
 }
 export default ItemDialog;

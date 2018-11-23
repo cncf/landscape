19f296c78b9df19e2bb993bb275338df69508f74
ZeusTheTrueGod <l-2732@yandex.ru>
2018-11-24 00:40:08 +0300

    do not forget to require a twitter js

--- src/components/ItemDialogContent.js
+++ src/components/ItemDialogContent.js
@@ -123,8 +123,15 @@ function handleDown() {
   productScrollEl.scrollBy({top: 200, behavior: 'smooth' });
 }
 
+const $script = require('scriptjs'); // eslint-disable-line global-require
+$script('https://platform.twitter.com/widgets.js', 'twitter-widgets');
+
 
 const ItemDialogContent = ({itemInfo}) => {
+
+
+
+
   const linkToOrganization = filtersToUrl({grouping: 'organization', filters: {organization: itemInfo.organization}});
   const itemCategory = function(path) {
     var separator = <span className="product-category-separator">•</span>;

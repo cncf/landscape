8782edc0ceeda862853b0124e3bb24ba46754f9f
Andrey Kozlov <l-2732@yandex.ru>
2018-11-23 16:10:03 +0300

    942 detect redirects (#991)
    * work in progress
    
    * wip
    
    * a test to ensure that puppeteer can properly detect an http redirect
    
    * wip
    
    * keep displaying dots
    
    * properly displaying messages
    
    * exclude sandbox projects from a preview (#990)
    
    * adding a cards animation and a big picture animation; closes #283 (#985)
    
    * adding a fade animation for the big picture
    
    * experimenting
    
    * adding a simple fade in / fade out animation
    
    * animating only first 30 items
    
    * further progress
    
    * first experiment with moving cards on sort
    
    * fix an issue with an item which is not moving
    
    * do not run an animation at all when not required, try to minimize width and height changing
    
    * fixing bugs with hidden items
    
    * an animation for items far below
    
    * almost perfect animation
    
    * more optimization, rendering first components quickly and delaying the render of remaining components
    
    * more synchronized animation, scale instead of opacity
    
    * typo
    
    * one more attempt
    
    * and again
    
    * better headers animation
    
    * fix header margins during the animation
    
    * a proper timeout
    
    * a bit better animation
    
    * no movement animation for elements out of the viewport
    
    * no more slide in or slide out
    
    * optimizations, code cleanup
    
    * keep a previous animation style. increase the limit of animated elements up to 100
    
    * completely hide old elements after an animation
    
    * Update out-of-date links
    
    Signed-off-by: Dan Kohn <dan@dankohn.com>

--- tools/checkLinks.js
+++ tools/checkLinks.js
@@ -5,11 +5,14 @@ import Promise from 'bluebird';
 const traverse = require('traverse');
 import retry from './retry';
 
+const fatal = (x) => colors.red(colors.inverse(x));
 const rpWithRetry = async function(args) {
   return await retry(() => rp(args), 2, 1000);
 }
 
-const fatal = (x) => colors.red(colors.inverse(x));
+process.setMaxListeners(0);
+
+
 
 async function getLandscapeItems() {
   const source = require('js-yaml').safeLoad(require('fs').readFileSync('landscape.yml'));
@@ -22,12 +25,12 @@ async function getLandscapeItems() {
     if (node.item !== null) {
       return;
     }
-    items.push({homepageUrl: node.homepage_url, name: node.name});
+    items.push({homepageUrl: node.homepage_url, repo: node.repo_url, name: node.name});
   });
   return _.uniq(items);
 }
 
-async function main() {
+async function checkUrl(url) {
   function getFullLocation(url, redirect) {
     if (redirect.indexOf('http') === 0) {
       return redirect;
@@ -36,71 +39,120 @@ async function main() {
     const myURL = new URL(url);
     return `${myURL.protocol}//${myURL.host}${redirect}`;
   }
+
+  async function checkViaPuppeteer() {
+    const puppeteer = require('puppeteer');
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setRequestInterception(true);
+    page.on('request', request => {
+      if (request.resourceType() !== 'document')
+        request.abort();
+      else
+        request.continue();
+    });
+    page.setDefaultNavigationTimeout(120 * 1000);
+    let result = null;
+
+    try {
+      page.on('response', response => {
+        const status = response.status()
+        if (url === response.request().url() || url + '/' === response.request().url) {
+          if ((status >= 300) && (status <= 399)) {
+            result = {type: 'redirect', redirect: response.headers()['location']};
+          }
+          else if (status >= 400) {
+            result = {type: 'error', status: status};
+          }
+        }
+      })
+      await page.goto(url);
+      result = result || 'ok';
+      await browser.close();
+      return result;
+    } catch(ex2) {
+      await browser.close();
+      return {type: 'error', message: ex2.message.substring(0, 200)};
+    }
+  }
+
+  async function checkWithRequest() {
+    const result = await rpWithRetry({
+      followRedirect: false,
+      url: url,
+      timeout: 45 * 1000,
+      simple: false,
+      resolveWithFullResponse: true,
+      headers: { // make them think we are a real browser from us
+        accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'accept-encoding': 'gzip, deflate, br',
+        'accept-language': 'en-US,en;q=0.9,es',
+        'cache-control': 'no-cache',
+        dnt: '1',
+        pragma: 'no-cache',
+        'upgrade-insecure-requests': 1,
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
+      }
+    });
+    if (result.statusCode === 200) {
+      return 'ok';
+    }
+    else if (result.statusCode >= 300 && result.statusCode < 400) {
+      return { type: 'redirect', location: getFullLocation(url, result.headers.location)};
+    } else {
+      return {type: 'error', status: result.statusCode};
+    }
+  }
+
+  try {
+    return await checkWithRequest();
+  } catch (ex) {
+    return await checkViaPuppeteer();
+  }
+}
+
+function formatError(record) {
+  if (record.type === 'redirect' && record.homepageUrl) {
+    return `homepage ${record.homepageUrl} redirects to ${record.location}`;
+  }
+  if (record.type === 'redirect' && record.repo) {
+    return `repo ${record.repo} redirects to ${record.repo}`;
+  }
+  if (record.type === 'error') {
+    const kind = record.homepageUrl ? `homepage ${record.homepageUrl}` : `repo ${record.repo}`;
+    const statusPart = record.status ? `has a status ${record.status}` : null;
+    const messagePart = record.message ? `has an error ${record.message}` : null;
+    const info = [statusPart, messagePart].filter( (x) => !!x).join(' and ');
+    return `${kind} ${info}`;
+  }
+
+}
+
+async function main() {
   const items = await getLandscapeItems();
   const errors= [];
   await Promise.map(items, async function(item) {
-    const checkViaPuppeteer = async function() {
-      const puppeteer = require('puppeteer');
-      const browser = await puppeteer.launch();
-      const page = await browser.newPage();
-      const itemCopy = item;
-      page.on('response', async(response) => {
-        if (itemCopy.processed) {
-          return;
-        }
-        itemCopy.processed = true;
-        if (response._status >= 200 && response._status < 300) {
-          require('process').stdout.write(".");
-        }
-        else {
-          errors.push(`${itemCopy.name} has an url ${itemCopy.homepageUrl} and the response code is ${response._status}`);
-          require('process').stdout.write(fatal("F"));
-        }
-      });
-      try {
-        await page.goto(itemCopy.homepageUrl, { waitUntil: 'networkidle2', timeout: 60000  });
-        await browser.close();
-      } catch(ex2) {
-        errors.push(`${item.name} has an url ${item.homepageUrl} and the reason is ${ex2.message.substring(0, 200)}`);
-        require('process').stdout.write(fatal("F"));
-      }
+    const result = await checkUrl(item.homepageUrl);
+    if (result !== 'ok') {
+      errors.push({'homepageUrl': item.homepageUrl,...result});
+      require('process').stdout.write(fatal("F"));
+    } else {
+      require('process').stdout.write(".");
     }
-    const checkWithRequest = async function() {
-      const result = await rpWithRetry({
-        followRedirect: false,
-        url: item.homepageUrl,
-        timeout: 45 * 1000,
-        simple: false,
-        resolveWithFullResponse: true,
-        headers: { // make them think we are a real browser from us
-          accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-          'accept-encoding': 'gzip, deflate, br',
-          'accept-language': 'en-US,en;q=0.9,es',
-          'cache-control': 'no-cache',
-          dnt: '1',
-          pragma: 'no-cache',
-          'upgrade-insecure-requests': 1,
-          'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
-        }
-      });
-      if (result.statusCode !== 200) {
-        let extra = '';
-        if (result.statusCode >= 300 && result.statusCode < 400) {
-          extra = `to ${getFullLocation(item.homepageUrl, result.headers.location)}`;
-        }
-        errors.push(`${item.name} has an url ${item.homepageUrl} and the response code is ${result.statusCode} ${extra}`);
+  }, {concurrency: 25});
+  await Promise.map(items, async function(item) {
+    if (item.repo) {
+      const result = await checkUrl(item.repo);
+      if (result !== 'ok') {
+        errors.push({'repo': item.repo, ...result});
         require('process').stdout.write(fatal("F"));
       } else {
         require('process').stdout.write(".");
       }
     }
-    try {
-      await checkWithRequest();
-    } catch (ex) {
-      await checkViaPuppeteer();
-    }
-  }, {concurrency: 20});
-  _.uniq(errors).forEach((x) => console.info(x));
+  }, {concurrency: 25});
+  console.info('');
+  _.uniq(errors).forEach((x) => console.info(formatError(x)));
   process.exit(errors.length === 0 ? 0 : 1);
 }
 main();

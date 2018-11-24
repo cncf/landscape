4baccc26e73d902be66854fb842f3d8b9d6d63cb
Dan Kohn <dan@linuxfoundation.org>
2018-11-24 13:23:08 -0500

    Shake the tree more; closes #994 (#999)
    * use a single lodash lib
    
    * do not include extra twitter widgets
    
    * use only the required iframe resizer file
    
    * do not forget to require a twitter js

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -33,6 +33,9 @@ export default {
     extensions: ['*', '.js', '.jsx', '.json'],
     alias: {
       '@material-ui/core': '@material-ui/core/es',
+      'lodash.clonedeep': 'lodash/cloneDeep.js',
+      'lodash.set': 'lodash/set.js',
+      'lodash.get': 'lodash/get.js',
       'current-device': 'current-device/es',
       'react-redux': 'react-redux/es',
       'react-router-dom': 'react-router-dom/es',

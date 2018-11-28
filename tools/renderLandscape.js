4942a2891df41449f20b97960cbeb671c46c254d
ZeusTheTrueGod <l-2732@yandex.ru>
2018-11-28 21:32:51 +0300

    update a hight for the rendered image

--- tools/renderLandscape.js
+++ tools/renderLandscape.js
@@ -15,7 +15,7 @@ async function main() {
   const puppeteer = require('puppeteer');
   const pages = [{
     url: `/landscape?preview&version=${version}`,
-    size: {width: 4900, height: 3240, deviceScaleFactor: 1},
+    size: {width: 4900, height: 2750, deviceScaleFactor: 1},
     fileName: 'src/images/landscape.png',
     pdfFileName: 'src/images/landscape.pdf'
   }];

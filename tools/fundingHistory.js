ca8909f29be2defd7a3f1883909a32e98175fbc1
ZeusTheTrueGod <l-2732@yandex.ru>
2018-11-30 19:32:18 +0300

    adding a version which shows 20 latest changes

--- tools/fundingHistory.js
+++ tools/fundingHistory.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
-function getFileFromHistory() {
-  const content = require('child_process').execSync('git show origin/master@{2018-11-21}:processed_landscape.yml').toString('utf-8');
+function getFileFromHistory(days) {
+  const commit = require('child_process').execSync(`git rev-list -n 1 --before='{${days} days ago}' origin/master`).toString('utf-8').trim();
+  const content = require('child_process').execSync(`git show ${commit}:processed_landscape.yml`).toString('utf-8');
   const source = require('js-yaml').safeLoad(content);
   return source;
 }
@@ -15,16 +16,16 @@ const getItems = function(yaml) {
   return _.flattenDeep( yaml.landscape.map( (category) => category.subcategories.map( (sc) => sc.items)));
 }
 
-const prev = getItems(getFileFromHistory());
-const current = getItems(getFileFromHead());
 
-function buildDiff(currentItems, prevItems) {
-  const result = [];
+function buildDiff({currentItems, prevItems, date, result}) {
   _.each(currentItems, function(item) {
     if (!item.crunchbase_data) {
       return;
     }
     if (!item.crunchbase_data.funding) {
+      return;
+    }
+    if (_.find(result, {name: item.crunchbase_data.name})) {
       return;
     }
     const previousEntry = _.find(prevItems, (prevItem) => prevItem.crunchbase_data && prevItem.crunchbase_data.name === item.crunchbase_data.name);
@@ -33,26 +34,33 @@ function buildDiff(currentItems, prevItems) {
         name: item.crunchbase_data.name,
         currentAmount: item.crunchbase_data.funding,
         previousAmount: previousEntry.crunchbase_data.funding,
-        date: 'test',
-        link: 'todo',
-        url: item.crunchbase
-      });
-    } else if (!previousEntry) {
-      result.push({
-        name: item.crunchbase_data.name,
-        currentAmount: item.crunchbase_data.funding,
-        previousAmount: '',
-        date: 'today',
+        date: date,
         link: 'todo',
         url: item.crunchbase
       });
     }
   });
-  return result;
 }
 
-const diff = buildDiff(current, prev);
-console.info(diff);
+const result = [];
+const maxEntries = 20;
+_.range(1, 100).forEach(function(i) {
+  if (result.length >= maxEntries) {
+    return false;
+  }
+  const prev = getItems(getFileFromHistory(i));
+  const current = getItems(getFileFromHead());
+  console.info('day #', i);
+  console.info(prev.length);
+  console.info(result.length);
+  buildDiff({
+    currentItems: current,
+    prevItems: prev,
+    date: new Date( new Date().getTime() - 86400 * 1000 * i).toISOString().substring(0, 10),
+    result: result
+  });
+});
+console.info(result);
 
 
 

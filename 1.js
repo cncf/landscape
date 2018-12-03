6f83327f7cad9f2d4e6fee1d16c066f91203ecc4
ZeusTheTrueGod <l-2732@yandex.ru>
2018-12-04 00:18:53 +0200

    more progress

--- 1.js
+++ 1.js
@@ -3,9 +3,6 @@ require('@babel/register')({
         var ignore = false
         if (filename.match('/node_modules/') && !filename.match(/interactive-landscape/)) {
             ignore = true;
-            console.info('ignoring: ', filename);
-        } else {
-            console.info(filename);
         }
         return ignore
     }]

b8051304236ff79a11481a93a2c1d87919e16b72
Dan Kohn <dan@dankohn.com>
2018-12-07 11:54:25 -0500

    Ban most paths in robots.txt
    Signed-off-by: Dan Kohn <dan@dankohn.com>

--- tools/addRobots.js
+++ tools/addRobots.js
@@ -5,7 +5,14 @@ const isMainBranch = currentBranch === 'master';
 const content = isMainBranch ?
   `
 User-agent: *
-Disallow:
+Disallow: *grouping=
+Disallow: *sort=
+Disallow: *landscape=
+Disallow: *cncf=
+Disallow: *license=
+Disallow: *organization=
+Disallow: *headquarters=
+Disallow: *format=
 
 Sitemap: http://landscape.cncf.io/sitemap.xml
   `

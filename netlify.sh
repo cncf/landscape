2f97ae7f5b11fc285c2b2d31af69053c779fc0a4
Andrey Kozlov <l-2732@yandex.ru>
2020-05-30 23:40:07 +0100

    use a landscapeapp based build

--- netlify.sh
+++
@@ -1,1 +0,0 @@
-npm install -g npm && npm install && npm run build
---
+++ netlify/netlify.sh
@@ -0,0 +1,10 @@
+. ~/.nvm/nvm.sh
+npm pack interactive-landscape@latest
+tar xzf interactive*
+cd package
+cp -r ../node_modules .
+nvm use `cat .nvmrc`
+npm install -g npm
+npm install
+cp -r node_modules/* ../node_modules
+PROJECT_PATH=.. npm run build

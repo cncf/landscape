935528aa6bcd311c21f2e3ec78759d6c4aae811d
Andrey Kozlov <l-2732@yandex.ru>
2020-05-31 01:23:15 +0100

    Making this build compatible with a weekly update (#1696)
    * test with a weekly update
    
    * use a landscapeapp based build
    
    * oops
    
    * one more fix
    
    * build
    
    * ensure that we can build it for a latest update
    
    * final check

---
+++ netlify/netlify.sh
@@ -0,0 +1,11 @@
+. ~/.nvm/nvm.sh
+npm pack interactive-landscape@latest
+tar xzf interactive*
+cd package
+cp -r ../node_modules .
+nvm install `cat .nvmrc`
+nvm use `cat .nvmrc`
+npm install -g npm
+npm install
+cp -r node_modules/* ../node_modules
+PROJECT_PATH=../.. npm run build
